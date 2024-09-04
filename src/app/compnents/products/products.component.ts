6dadf06478f38cd0df081f93534363cf4e9d6a6d
shariar khan <kshariare@outlook.com>
2024-09-04 16:46:28 +0600

    Product Sorting and Filterring based on thier name has been implemented.

--- src/app/compnents/products/products.component.ts
+++ src/app/compnents/products/products.component.ts
@@ -1,14 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import Product from '../../types/product';
+import { ProductService } from '../../services/product.service';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule,MatSort } from '@angular/material/sort';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink,MatButtonModule],
+  imports: [
+    RouterLink,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatInputModule,
+  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.css'
+  styleUrl: './products.component.css',
 })
 export class ProductsComponent {
-
+  datasource!: MatTableDataSource<Product>;
+  productService = inject(ProductService);
+  products: Product[] = [];
+  displayedColumns = [
+    'name',
+    'details',
+    'brandId',
+    'purchasedPrice',
+    'salesPrice',
+    'availableQuantity',
+    'action',
+  ];
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  ngOnInit() {
+    this.productService.getProducts().subscribe((result) => {
+      this.products = result;
+      this.initTable();
+    });
+  }
+  initTable() {
+    this.datasource = new MatTableDataSource(this.products);
+     this.datasource.paginator = this.paginator;
+     this.datasource.sort = this.sort;
+  }
+  applyFilter(event: Event) {
+        this.datasource.filter = (event.target as HTMLInputElement).value
+          .trim()
+          .toLowerCase();
+        this.paginator.firstPage();
+  }
 }
